ea4e6910fd00a66fa71b8e72b8fbd3edd907b5c9
Hieu Nguyen <hieunct2001@gmail.com>
2023-03-30 00:37:17 -0400

    Fix url

--- backend/index.php
+++ backend/index.php
@@ -20,15 +20,17 @@ $uri_len = count($uri);
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 // pass the request method and user ID to the PersonController and process the HTTP request:
 
+if ($uri[$uri_len - 2] === "sport") {
+    $sportController = new \Api\SportController($requestMethod, $uri[$uri_len - 1]);
+    $sportController->processRequest();
+}
+
 if ($uri[$uri_len - 2] === "class") {
     $classController = new \Api\ClassController($requestMethod, $uri[$uri_len - 1]);
     $classController->processRequest();
 }
 
-if ($uri[$uri_len - 2] === "sport") {
-    $sportController = new \Api\SportController($requestMethod, $uri[$uri_len - 1]);
-    $sportController->processRequest();
-}
+
 
 if ($uri[$uri_len - 2] === "weather") {
     $weatherController = new \Api\WeatherController($requestMethod, $uri[$uri_len - 1]);

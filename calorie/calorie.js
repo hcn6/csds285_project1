63ef87063c351584ad02fadfab3745ad680bd7e3
Hieu Nguyen <hieunct2001@gmail.com>
2023-04-04 18:38:08 -0400

    Add Enter key to submit data

--- calorie/calorie.js
+++ calorie/calorie.js
@@ -1,4 +1,17 @@
-const intakeList = [];
+// Get the input element
+const foodInput = document.getElementById("food");
+const foodButton = document.getElementById("food-button")
+
+// Add an event listener for the "keydown" event
+foodInput.addEventListener("keydown", function(event) {
+  // Check if the Enter key was pressed
+  if (event.key === "Enter") {
+    // Prevent the default action of the Enter key (i.e., submitting the form)
+    event.preventDefault();
+    // Submit the form
+    foodButton.click()
+  }
+});
 
 function queryNutritionFact() {
     // const local_url = "http://localhost:8000/calories/nutritionFact";
@@ -16,9 +29,23 @@ function queryNutritionFact() {
         .then(response => response.json())
         .then(data => {
             clearContentById('result');
-            createTable(data);
+            if(data.length > 0) {
+                createTable(data);
+            }
+            else {
+                showNoResultsMessage();
+            }
         })
 }
+
+function showNoResultsMessage() {
+    // Create a new element to hold the message
+    const messageElement = document.createElement("p");
+    // Set the text of the message
+    messageElement.textContent = "Sorry, no results were found.";
+    // Add the message to the page
+    document.body.appendChild(messageElement);
+  }  
 
 // Function to create a table from JSON data
 function createTable(data) {
